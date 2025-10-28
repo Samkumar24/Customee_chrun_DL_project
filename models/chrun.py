fe31cc636a3f1a2b113656bddad9fa34a2db970e
Samkumar24 <samkumarr24@gmail.com>
2025-10-28 08:13:52 +0530

    Updated requriments.txt file

--- models/chrun.py
+++ models/chrun.py
@@ -1,6 +1,20 @@
 import pandas as pd 
 import numpy as np 
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import EarlyStopping
+from tenflow.keras.models import load_model
 
-print("sam")
+df = pd.read_csv("C:\\Users\\sam\\Downloads\\bank_churn_data.csv")
+
+#print(df.columns)
+
+X = df.drop(['CustomerId','Surname','Exited'], axis=1)
+y = df['Exited']
+
+print(X.columns,  y)
 
 
